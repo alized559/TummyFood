b79be7b195f322b7a477964d6529a1d158b84324
Ali Zein Al Dine <alized559@gmail.com>
2021-12-23 12:59:10 +0200

    adding splash screen

---
+++ app/src/main/java/com/example/tummyfood/SplashScreen.java
@@ -0,0 +1,23 @@
+package com.example.tummyfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    public static final int TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed((Runnable) () -> {
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        }, TIME);
+    }
+}
