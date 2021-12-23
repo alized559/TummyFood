f9c2e4de02ca065bc3b18e16bc324588bfaf450a
Ali Zein Al Dine <alized559@gmail.com>
2021-12-23 23:10:06 +0200

    .

--- app/src/main/java/com/example/tummyfood/RecipesActivity.java
+++ app/src/main/java/com/example/tummyfood/RecipesActivity.java
@@ -10,5 +10,7 @@ public class RecipesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
+
+        String currentCategory = getIntent().getStringExtra("currentCategory");
     }
 }
