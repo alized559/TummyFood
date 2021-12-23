341f623038cb82c2a018fce6b19ea3cf02dc6673
Ali Zein Al Dine <alized559@gmail.com>
2021-12-23 23:07:51 +0200

    adding categories

--- app/src/main/java/com/example/tummyfood/CategoriesActivity.java
+++ app/src/main/java/com/example/tummyfood/CategoriesActivity.java
@@ -3,6 +3,9 @@ package com.example.tummyfood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class CategoriesActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class CategoriesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+        ListView categoriesList = findViewById(R.id.categoriesList);
+
+        ArrayList<CategoriesDataModel> list = new ArrayList<>();
+        list.add(new CategoriesDataModel(R.drawable.apetizers, "Apetizers"));
+        list.add(new CategoriesDataModel(R.drawable.main_dish, "Main Dishes"));
+        list.add(new CategoriesDataModel(R.drawable.dessert, "Desserts"));
+
+        CategoriesListAdapter adapter = new CategoriesListAdapter(this, R.layout.categorieslistitem,
+                list);
+        categoriesList.setAdapter(adapter);
     }
 }
